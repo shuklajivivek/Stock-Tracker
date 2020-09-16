9394cafee2b63c6e79af4fc118bf40761a9dae09
vivekshukla <vivekshukla.nit@gmail.com>
2020-09-16 18:49:36 +0530

    Add many-to-many association between users and stocks

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,9 @@
 class Stock < ApplicationRecord
+  
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+  validates :name, :ticker, presence: true
+  
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:api_public_key],
                                   secret_token: Rails.application.credentials.iex_client[:api_secret_key],
@@ -8,5 +13,8 @@ class Stock < ApplicationRecord
     rescue => exception
     return nil
     end
+  end
+  def self.check_db(ticker_symbol)
+    where(ticker: ticker_symbol).first
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+resources :user_stocks, only: [:create]
+devise_for :users
 root 'welcome#index'
 get 'my_portfolio', to: 'users#my_portfolio'
 get 'search_stock', to: 'stocks#search'
